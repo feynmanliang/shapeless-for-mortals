4ed35f4dc551da99f9f4bd975441384699e23a63
Feynman Liang <feynman.liang@gmail.com>
2016-01-25 22:48:37 +0000

    Implements hlist formatter

--- smbd/src/main/scala/StringyMapBigData.scala
+++ smbd/src/main/scala/StringyMapBigData.scala
@@ -42,12 +42,57 @@ package object impl {
   import api._
 
   // EXERCISE 1.1 goes here
-  // implicit def hNilBigDataFormat = ???
-  // implicit def hListBigDataFormat = ???
-  // implicit def cNilBigDataFormat = ???
-  // implicit def coproductBigDataFormat = ???
+  implicit object hNilBigDataFormat extends BigDataFormat[HNil] {
+    def label: String = ""
+    def toProperties(t: HNil): StringyMap = new StringyMap()
+    def fromProperties(m: StringyMap): BigResult[HNil] = Right(HNil)
+  }
+  implicit def hListBigDataFormat[Key <: Symbol, Value, Remaining <: HList](
+    implicit
+    key: Witness.Aux[Key],
+    sp: SPrimitive[Value],
+    lazyBdft: Lazy[BigDataFormat[Remaining]]
+  ): BigDataFormat[FieldType[Key, Value] :: Remaining] = new BigDataFormat[FieldType[Key, Value] :: Remaining] {
+    val bdft = lazyBdft.value
+    def label: String = key.value.name
+    def toProperties(hlist: FieldType[Key, Value] :: Remaining): StringyMap = {
+      val m = bdft.toProperties(hlist.tail)
+      m.put(label, sp.toValue(hlist.head))
+      m
+    }
+    def fromProperties(m: StringyMap): BigResult[FieldType[Key, Value] :: Remaining] = {
+      val head = sp.fromValue(m.get(key.value.name))
+      m.remove(key.value.name)
+      bdft.fromProperties(m) match {
+        case Left(err) => Left(err)
+        case Right(tail) => Right(field[Key](head) :: tail)
+      }
+    }
+  }
+  implicit object cNilBigDataFormat extends BigDataFormat[CNil] {
+    def label: String = ???
+    def toProperties(t: CNil): StringyMap = ???
+    def fromProperties(m: StringyMap): BigResult[CNil] = ???
+  }
+  implicit def coproductBigDataFormat[Name <: Symbol, Head, Tail <: Coproduct](
+    implicit
+    key: Witness.Aux[Key],
+    sp: SPrimitive[Value],
+    lazyBdft: Lazy[BigDataFormat[Remaining]]
+  ): BigDataFormat[FieldType[Name, Head] :+: Tail] = new BigDataFormat[FieldType[Name, Head] :+: Tail] {
+    def label: String = ???
+    def toProperties(lr: FieldType[Name, Head] :+: Tail): StringyMap = lr match {
+      case Inl(found) => ???
+      case Inr(tail) => ???
+    }
+    def fromProperties(m: StringyMap): BigResult[FieldType[Name, Head] :+: Tail] = ???
+  }
 
-  implicit def familyBigDataFormat[T] = ???
+  implicit def familyBigDataFormat[T](): BigDataFormat[T] = new BigDataFormat[T] {
+    def label: String = ???
+    def toProperties(t: T): StringyMap = ???
+    def fromProperties(m: StringyMap): BigResult[T] = ???
+  }
 }
 
 package impl {
